bb4b2eb1ecea5eb419bdf2a1e5619616368831a8
Roei Boribor <roeiboribor@gmail.com>
2024-04-02 11:34:39 +0800

    changes to laravel passport

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,12 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        // Passport
+        'api' => [
+            'driver' => 'passport',
+            'provider' => 'users',
+        ],
     ],
 
     /*
