9da253d7f5fae1a341eb26e50d004c1aceb6511e
Roei Boribor <roeiboribor@gmail.com>
2024-04-02 17:01:08 +0800

    minor changes

--- app/Http/Controllers/Api/V1/LoginController.php
+++ app/Http/Controllers/Api/V1/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Authentication\LoginStoreRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -12,8 +14,20 @@ class LoginController extends Controller
         'message' => 'Oops Something went wrong!'
     ];
 
-    public function store()
+    public function store(LoginStoreRequest $request): JsonResponse
     {
+        $data = $request->validated();
+
+        if (auth()->attempt($data)) {
+            $this->response['status_code'] = 200; // Unauthorized
+            $this->response['message'] = 'You have successfully login!';
+            // $this->response['token'] = auth()->user()->createToken('authenticated')->accessToken;
+            $this->response['token'] = auth()->user()->createToken('authenticated')->accessToken;
+        } else {
+            $this->response['status_code'] = 401; // Unauthorized
+            $this->response['message'] = 'Invalid credentials';
+        }
+
         return response()->json([
             ...$this->response
         ]);
